7c7888bdfb6663736f6d14b244182cf16a48a06a
Bryce <71194251+darthflorb@users.noreply.github.com>
2022-09-29 12:34:19 -0700

    Update front_car.py

--- front_car.py
+++ front_car.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x1 = 0
+x2 = 40
+y1 = -10
+y2 = -50
+
+
 def front_car(plt, car_top, car_bottom, car_ground):
-    plt.scatter(50, -50, s = 2000, color = 'r')
+    plt.plot([x1, x2], [y1, y2], color = 'k')
