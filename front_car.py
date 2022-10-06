0ecbfa1b9da6d945dc5378ac1ea0965e46e54ced
Bryce <71194251+darthflorb@users.noreply.github.com>
2022-10-06 11:52:16 -0700

    Update front_car.py

--- front_car.py
+++ front_car.py
@@ -1,17 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-<<<<<<< Updated upstream
-def front_car(plt, axes, car_top, car_bottom, car_ground):
-    car_bounds = [10, 40]
-=======
 car_bounds = [10, 40]
 
 def front_car(plt, car_top, car_bottom, car_ground):
     car_body(plt, car_top, car_bottom, car_ground)
     
 def car_body(plt, car_top, car_bottom, car_ground):
->>>>>>> Stashed changes
     
     x = [car_bounds[0], car_bounds[1], car_bounds[1], car_bounds[0], car_bounds[0]]
     y = [car_top, car_top, car_bottom, car_bottom, car_top]
